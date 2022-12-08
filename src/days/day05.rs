8f8a51ed2eaf971886e30d95379de26c9d1e17f9
Lukas Winkler <derwinlu+git@gmail.com>
2022-12-08 15:40:25 +0100

    Impl day 5 + 6

--- src/days/day05.rs
+++ src/days/day05.rs
@@ -1,11 +1,158 @@
 use crate::{Solution, SolutionPair};
+use lazy_static::lazy_static;
+use regex::Regex;
+use std::fs;
 
-///////////////////////////////////////////////////////////////////////////////
+lazy_static! {
+    static ref MOVE_RE: Regex =
+        Regex::new(r"^move (?P<amount>\d+) from (?P<from>\d+) to (?P<to>\d+)$").unwrap();
+}
 
+fn solve_day1(input: &str) -> String {
+    let (start, instructions) = input.split_once("\n\n").unwrap();
+
+    let mut parse_start_setup_iter = start.lines().rev();
+    let last_line = parse_start_setup_iter.next().unwrap();
+    let mut stacks: Vec<Vec<char>> = last_line.split_whitespace().map(|_id| Vec::new()).collect();
+
+    for line in parse_start_setup_iter {
+        for (i, stack) in stacks.iter_mut().enumerate() {
+            if let Some(cra) = line.chars().nth(1 + i * 4) {
+                if cra.is_ascii_uppercase() {
+                    stack.push(cra);
+                }
+            };
+        }
+    }
+
+    for instruction in instructions.lines() {
+        let instr_parsed = MOVE_RE
+            .captures(instruction)
+            .expect(&format!("{instruction} could not be parsed"));
+        let amount: usize = instr_parsed
+            .name("amount")
+            .unwrap()
+            .as_str()
+            .parse()
+            .unwrap();
+        let from: usize = instr_parsed.name("from").unwrap().as_str().parse().unwrap();
+        let to: usize = instr_parsed.name("to").unwrap().as_str().parse().unwrap();
+        // println!("{stacks:?} {amount} {from} {to}");
+
+        let from_stack = stacks.get_mut(from - 1).unwrap();
+
+        let start = from_stack.len() - amount;
+        let mut to_move = from_stack.split_off(start);
+        to_move.reverse();
+
+        //println!("{from_stack:?} {} {start} {to_move:?}", from_stack.len());
+
+        let to_stack = stacks.get_mut(to - 1).unwrap();
+        to_stack.append(&mut to_move);
+    }
+
+    let result: String = stacks
+        .iter()
+        .map(|stack| stack.last().unwrap_or(&' '))
+        .collect();
+    // println!("{stacks:?}");
+    result
+}
+
+fn solve_day2(input: &str) -> String {
+    let (start, instructions) = input.split_once("\n\n").unwrap();
+
+    let mut parse_start_setup_iter = start.lines().rev();
+    let last_line = parse_start_setup_iter.next().unwrap();
+    let mut stacks: Vec<Vec<char>> = last_line.split_whitespace().map(|_id| Vec::new()).collect();
+
+    for line in parse_start_setup_iter {
+        for (i, stack) in stacks.iter_mut().enumerate() {
+            if let Some(cra) = line.chars().nth(1 + i * 4) {
+                if cra.is_ascii_uppercase() {
+                    stack.push(cra);
+                }
+            };
+        }
+    }
+
+    for instruction in instructions.lines() {
+        let instr_parsed = MOVE_RE
+            .captures(instruction)
+            .expect(&format!("{instruction} could not be parsed"));
+        let amount: usize = instr_parsed
+            .name("amount")
+            .unwrap()
+            .as_str()
+            .parse()
+            .unwrap();
+        let from: usize = instr_parsed.name("from").unwrap().as_str().parse().unwrap();
+        let to: usize = instr_parsed.name("to").unwrap().as_str().parse().unwrap();
+        // println!("{stacks:?} {amount} {from} {to}");
+
+        let from_stack = stacks.get_mut(from - 1).unwrap();
+
+        let start = from_stack.len() - amount;
+        let mut to_move = from_stack.split_off(start);
+        // to_move.reverse();
+
+        //println!("{from_stack:?} {} {start} {to_move:?}", from_stack.len());
+
+        let to_stack = stacks.get_mut(to - 1).unwrap();
+        to_stack.append(&mut to_move);
+    }
+
+    let result: String = stacks
+        .iter()
+        .map(|stack| stack.last().unwrap_or(&' '))
+        .collect();
+    // println!("{stacks:?}");
+    result
+}
 pub fn solve() -> SolutionPair {
-    // Your solution here...
-    let sol1: u64 = 0;
-    let sol2: u64 = 0;
+    let input = fs::read_to_string("src/days/day05.txt").expect("File should not fail");
+    let sol1: String = solve_day1(&input);
+    let sol2: String = solve_day2(&input);
 
     (Solution::from(sol1), Solution::from(sol2))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use indoc::indoc;
+
+    #[test]
+    fn sample_day_1() {
+        let input = indoc! {"
+                [D]
+            [N] [C]
+            [Z] [M] [P]
+            1   2   3
+
+            move 1 from 2 to 1
+            move 3 from 1 to 3
+            move 2 from 2 to 1
+            move 1 from 1 to 2
+        "};
+        let result = solve_day1(input);
+        assert_eq!(result, "CMZ");
+    }
+
+    #[test]
+    fn sample_day_2() {
+        let input = indoc! {"
+                [D]
+            [N] [C]
+            [Z] [M] [P]
+            1   2   3
+
+            move 1 from 2 to 1
+            move 3 from 1 to 3
+            move 2 from 2 to 1
+            move 1 from 1 to 2
+        "};
+        let result = solve_day2(input);
+        assert_eq!(result, "MCD");
+    }
 }
--- src/days/day06.rs
+++ src/days/day06.rs
@@ -1,11 +1,64 @@
+use std::{collections::HashSet, fs};
+
 use crate::{Solution, SolutionPair};
 
-///////////////////////////////////////////////////////////////////////////////
+fn generic_solver(input: &str, window_size: usize) -> u64 {
+    let windows = input.as_bytes().windows(window_size);
+    for (offset, window) in windows.enumerate() {
+        let mut uniq = HashSet::new();
+        let is_uniq = window.into_iter().all(|x| uniq.insert(x));
+        if is_uniq {
+            return (offset + window_size) as u64;
+        }
+    }
+    unreachable!()
+}
+fn solve_day1(input: &str) -> u64 {
+    generic_solver(input, 4)
+}
 
+fn solve_day2(input: &str) -> u64 {
+    generic_solver(input, 14)
+}
 pub fn solve() -> SolutionPair {
-    // Your solution here...
-    let sol1: u64 = 0;
-    let sol2: u64 = 0;
+    let input = fs::read_to_string("src/days/day06.txt").expect("File should not fail");
+    let sol1 = solve_day1(&input);
+    let sol2 = solve_day2(&input);
 
     (Solution::from(sol1), Solution::from(sol2))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn sample_day_1() {
+        let inputs = vec![
+            ("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7),
+            ("bvwbjplbgvbhsrlpgdmjqwftvncz", 5),
+            ("nppdvjthqldpwncqszvftbrmjlhg", 6),
+            ("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10),
+            ("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11),
+        ];
+        for (input, expected) in inputs {
+            let result = solve_day1(input);
+            assert_eq!(result, expected);
+        }
+    }
+
+    #[test]
+    fn sample_day_2() {
+        let inputs = vec![
+            ("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 19),
+            ("bvwbjplbgvbhsrlpgdmjqwftvncz", 23),
+            ("nppdvjthqldpwncqszvftbrmjlhg", 23),
+            ("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 29),
+            ("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 26),
+        ];
+        for (input, expected) in inputs {
+            let result = solve_day2(input);
+            assert_eq!(result, expected);
+        }
+    }
 }
